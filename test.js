9686291f9c7046b664e6d26b67016bb1ab6926fc
Tashiro <Tashiro-Gyori@users.noreply.github.com>
2022-04-20 14:58:47 +0200

    Fix test selenium

--- test.js
+++ test.js
@@ -8,19 +8,22 @@ options.setPreference("browser.helperApps.neverAsk.saveToDisk", "application/oct
 // Instanciation of Firefox web driver (geckodriver)
 const driver = new Builder().forBrowser('firefox').setFirefoxOptions(options).build();
 driver.get('https://tashiro-gyori.github.io/pong_gba/'); // prob ici : teste la branche master...
-driver.findElement(By.id('screenshot-btn')).then(btn => {
+driver.findElement(By.id('screenshot-btn')).then(async btn => {
     btn.click();
-
-    driver.Navigate().GoToUrl("file:///home/ubuntu/Downloads/screenshot.png");
-    let file1 = driver.PageSource;
-    driver.Navigate().GoToUrl("https://raw.githubusercontent.com/Tashiro-Gyori/pong_gba/dev/tests/0.png"); // a changer après merge
-    let file2 = driver.PageSource;
-    if (file1.Equals(file2))
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
+    try {
+        driver.navigate().to("file:///home/ubuntu/Downloads/screenshot.png");
+        let file1 = await driver.getPageSource();
+        driver.navigate().to("https://raw.githubusercontent.com/Tashiro-Gyori/pong_gba/master/tests/0.png");
+        let file2 = await driver.getPageSource();
+        if (file1 === file2)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    } catch (error) {
+        console.error(error);
     }
 });
